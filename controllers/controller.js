b9570fbd282ce9da2faa2b539a293d58d64a5894
Bouba <diarraboubaca@yahoo.fr>
2023-03-15 17:51:25 +0100

    Implémentation du controller de connexion

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../model/model");
 const userValidation = require("../validation/validation");
 
@@ -12,7 +13,7 @@ exports.inscription = (req, res) => {
     // ** Récupérer les données
     const {body} = req;
     // ** Valider les données
-    const {error} = userValidation(body);
+    const {error} = userValidation(body).userValidationSignUp;
     if(error) return res.status(401).json(error.details[0].message)
 
     // hash du mdp
@@ -38,5 +39,27 @@ exports.inscription = (req, res) => {
  * @param {express.Response} res 
  */
 exports.connexion = (req, res) => {
-    res.send("connexion");
+    const {email, password} = req.body;
+    // ** validation des données
+    const {error} = userValidation(req.body).userValidationLogin
+    if(error) return res.status(401).json(error.details[0].message)
+
+    // ** trouver le user dans la base de données
+    User.findOne({email: email})
+    .then(user => {
+        if(!user) return res.status(404).json({msg: "User not found"})
+        // vérification du mdp
+        bcrypt.compare(password, user.password)
+        .then(match => {
+            if(!match) return res.status(500).json({msg: "Server error"})
+            // ** envoyer un token d'autentification au cas où ça marche
+            res.status(200).json({
+                email: user.email,
+                id: user._id,
+                token: jwt.sign({id: user._id}, "Secret_KEY", {expiresIn: "12h"})
+            })
+        })
+        .catch(error => res.status(500).json(error))
+    })
+    .catch(error => res.status(500).json(error))
 }
--- validation/validation.js
+++ validation/validation.js
@@ -1,14 +1,22 @@
 const joi = require("joi");
 
 function userValidation (body) {
-    const userValidationSchema = joi.object({
+    const userValidationSignUp = joi.object({
         firstName: joi.string().min(2).max(30).trim().required(),
         lastName: joi.string().min(2).max(30).trim().required(),
         email: joi.string().email().trim().required(),
         password: joi.string().min(5).max(30).required()
     })
 
-    return userValidationSchema.validate(body);
+    const userValidationLogin = joi.object({
+        email: joi.string().email().trim().required(),
+        password: joi.string().min(5).max(30).required()
+    })
+
+    return {
+        userValidationSignUp : userValidationSignUp.validate(body),
+        userValidationLogin : userValidationLogin.validate(body)
+    }
 }
 
 module.exports = userValidation
